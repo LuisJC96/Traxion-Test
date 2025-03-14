7671a175ca8f4f47a8f1c88674243333fb17a6c2
LuisJC96 <luisjuancenturion@gmail.com>
2025-03-13 22:52:00 -0600

    Fixed error

--- app/v1/exceptions/entity_not_found.py
+++ app/v1/exceptions/entity_not_found.py
@@ -3,5 +3,5 @@ from app.v1.exceptions.service_error import ServiceError
 
 class EntityNotFound(ServiceError):
     def __init__(self, db, collection, filter):
-        self.status_code = StatusCode.NOT_FOUND
-        self.details = f"Entity on db {db} and collection {collection} with filter {filter} Not Found"
+        self.status = StatusCode.NOT_FOUND
+        self.detail = f"Entity on db {db} and collection {collection} with filter {filter} Not Found"
