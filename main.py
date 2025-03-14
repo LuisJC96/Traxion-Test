d84589f6e51852ade17424e404224b8b06af6c34
LuisJC96 <luisjuancenturion@gmail.com>
2025-03-13 22:52:25 -0600

    Fixed singleton not working correctly

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from fastapi import FastAPI
 from app.v1.router import app as app_v1
 from contextlib import asynccontextmanager
 from tools.mongodb_singleton import MongoDBSingleton
-app = FastAPI()
-app.mount("/v1", app_v1)
+
 
 @asynccontextmanager
-async def db_manager():
+async def lifespan(app: FastAPI):
     MongoDBSingleton.get_client()
     yield
-    MongoDBSingleton.close()
+    MongoDBSingleton.close_client()
+
+
+app = FastAPI(lifespan=lifespan)
+app.mount("/v1", app_v1)
 
 if __name__ == "__main__":
     conf = Config(
