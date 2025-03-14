56354db9e37bc58033c20e0d5dadde92a97f1e12
LuisJC96 <luisjuancenturion@gmail.com>
2025-03-14 02:36:01 -0600

    Fixed typos

--- app/v1/exceptions/invalid_state_change.py
+++ app/v1/exceptions/invalid_state_change.py
@@ -1,8 +1,7 @@
 from enums.status_codes import StatusCode
 from app.v1.exceptions.service_error import ServiceError
 
-class EntityNotFound(ServiceError):
-    def __init__(self, origin_state, required_state, possible_transitions):
-        self.status_code = StatusCode.BAD_REQUEST
-        self.details = (f"Unable to transition from {origin_state} to {required_state} "
-                        f"The possible transitions are: {possible_transitions}")
+class InvalidStateChange(ServiceError):
+    def __init__(self, origin_state, required_state):
+        self.status = StatusCode.BAD_REQUEST
+        self.detail = f"Unable to transition from {origin_state} to {required_state}."
