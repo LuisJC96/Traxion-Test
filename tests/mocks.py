c1691b35d93739bd3aa55343e80283ff67b58235
LuisJC96 <luisjuancenturion@gmail.com>
2025-03-14 02:53:29 -0600

    Added mocks

---
+++ tests/mocks.py
@@ -0,0 +1,58 @@
+VEHICLE = {
+  "_id": "c1c60720-51c4-5319-8924-ff4e0c9e0ae8",
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 2025,
+  "is_active": True,
+  "registration": {
+    "country": "Mexico",
+    "state": "Ciudad de México",
+    "plate_number": "VPR-910"
+  },
+  "created_at": "2025-03-13 23:23:13",
+  "updated_at": "2025-03-13 23:23:13"
+}
+
+SERVICE_ORDER = {
+  "_id": "3aeb6883-19d9-4712-944d-dfe830ad39b1",
+  "state": "ASSIGNED",
+  "vehicle": {
+    "_id": "c1c60720-51c4-5319-8924-ff4e0c9e0ae8",
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 2025,
+    "is_active": True,
+    "registration": {
+      "country": "Mexico",
+      "state": "Ciudad de México",
+      "plate_number": "VPR-910"
+    },
+    "created_at": "2025-03-13 23:23:13",
+    "updated_at": "2025-03-13 23:23:13"
+  },
+  "updated_at": "2025-03-14 02:41:43",
+  "millage": 1000,
+  "notes": [
+    "Urgent service required.",
+    "Se lo comio el perro",
+    "Dos veces",
+    "Se lo comio el perro",
+    "Dos veces",
+    "Se lo comio el perro",
+    "Dos veces",
+    "Se lo comio el perro",
+    "Dos veces",
+    "Se lo comio el perro",
+    "Dos veces",
+    "Se lo comio el perro",
+    "Dos veces"
+  ],
+  "technician": None,
+  "scheduled_date": None,
+  "service_date": None,
+  "completion_date": None,
+  "service_type": "Oil change",
+  "service_description": "Rutine oil change",
+  "involved_parts": [],
+  "spare_parts": []
+}
